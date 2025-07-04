bf6d12641c2294009f0af87a082e1f5862d805cf
Delion <21adtydwf@gmail.com>
2025-07-04 16:24:28 +0700

    update: menambah materi tipe data.

---
+++ Python/Fundamental/tipe-data.py
@@ -0,0 +1,30 @@
+# Integer
+fn : int = 12
+ft : int = -21
+print(ft);print(fn)
+print(type(fn));print(type(ft))
+
+# String
+sn : str = "Hello World"
+st : str = "Delissesu Works"
+print(sn);print(st)
+print(type(sn));print(type(st))
+
+# Float
+fnn : float = 12.5
+fnt : float = -21.5
+print(fnn);print(fnt)
+print(type(fnn));print(type(fnt))
+
+# Boolean
+fb : bool = True
+tb : bool = False
+print(fb);print(ft)
+print(type(fb));print(type(tb))
+
+# Complex
+fcn : complex = 3 + 2j
+tcn : complex = 21 - 2j
+print(fcn);print(tcn)
+print(type(fcn));print(type(tcn))
+print(fcn.real);print(tcn.imag) #real and imaginary number partition
