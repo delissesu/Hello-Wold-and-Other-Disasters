548f68b804dcded53603690ee8e02789be0c15ec
Delion <21adtydwf@gmail.com>
2025-07-07 19:12:02 +0700

    update: learn how to format code better

---
+++ Python/Fundamental/formatting-code.py
@@ -0,0 +1,13 @@
+# Menggunakan black, yapf, dan autopep8
+
+class Kalkulator:
+    """kalkulator tambah kurang"""
+
+    def __init__(self, _i):
+        self.i = _i
+
+    def tambah(self, _i):
+        return self.i + _i
+
+    def kurang(self, _i):
+        return self.i - _i
