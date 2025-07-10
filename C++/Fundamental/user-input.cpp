0bb0fbfa9c5a1311727dd7d36e0fb8cbe756f9e8
Delion <21adtydwf@gmail.com>
2025-07-10 17:46:27 +0700

    update: basic of user input

---
+++ C++/Fundamental/user-input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    short a;
+    cout << "Input a random short number: " ;
+    cin >> a;
+    cout << "Its value is: "; ;
+    cout << a;
+    return 0;
+}
