6fa67752d09911c05f6202bf85068689b9fa6985
Delion <21adtydwf@gmail.com>
2025-10-28 14:45:15 +0700

    update: channel

--- Golang/channel/go-channel.go
+++ Golang/channel/go-channel.go
@@ -18,10 +18,33 @@ import "fmt"
 
 type Person struct {}
 
+func sum(s []int, c chan int) {
+    sum := 0
+
+    for _, v := range s {
+        sum += v
+    }
+    // send sum ke c    
+    c <- sum
+}
+
 func main() {
     // Cara inisiasi channel dengan menggunakan built in function make
     // c:=make(chan int)
     // Hasil kembalian dari channel adalah sebuah address
-    c := make(chan Person) // Tiped atanya juga bisa menggunakan struct
-    fmt.Println(c) 
+    // c := make(chan Person) // Tiped atanya juga bisa menggunakan struct
+    // fmt.Println(c)
+
+    // Cara untuk send dan receive value dari sebuah channel
+    s := []int{12, 21, 35, 65, 34, 80, 32, 45, 87, 90}
+
+    c := make(chan int)
+    go sum(s[len(s)/2:], c)
+    go sum(s[:len(s)/2], c)
+
+    // terima value dari c
+    x, y := <-c, <-c
+
+    fmt.Println(x, y)
+
 }
