fd0219c213bf7e0869309daeb790ed24637ec8c6
Delion <21adtydwf@gmail.com>
2025-11-06 09:26:33 +0700

    update: ntah, lama

---
+++ Python/Fundamental/looping.py
@@ -0,0 +1,28 @@
+# K = 1
+
+# dict_nama = {
+#     1 : "Delion",
+#     2 : "Naveria",
+#     3 : "Liliaveria"
+# }
+
+# while K < 4:
+#     print("Halo, Kak", dict_nama[K])
+#     K += 1
+# print("Sudah keluar")
+
+# daftar_angka : list[int] = [1, 2, 3, 4]
+
+# for i in range(len(daftar_angka)):
+#     # daftar_angka[i] += 1
+#     daftar_angka[i] **=2
+# print(daftar_angka)
+# counter = 1
+# while True:
+#     print("Anything u want")
+#     counter += 1
+#     if counter == 10:
+#         print("Ini angka 10")
+#         continue
+#     elif counter == 11:
+#         break
