a119e81ad1e2cef5c8d7dcca1b9923bbef4589f0
Delion <21adtydwf@gmail.com>
2025-11-09 22:58:00 +0700

    update: belajar handle get request

--- JavaScript/Project/data-server.js
+++ JavaScript/Project/data-server.js
@@ -1,13 +1,35 @@
 // Hari 3: "Data Handler" - Processing Request Data & POST Handling
 
-const HTTP = require('http');
-const QUERY_STRING = require('querystring');
+const http = require('http');
+const querystring = require('querystring');
+const { URLSearchParams } = require('url');
 
-const SERVER = HTTP.createServer((request, response) => {
-    const {
-        url,
-        method
-    } = request;
+const server = http.createServer((request, response) => {
+    const { url, method } = request;
 
     console.log(`${method} : ${url}`);
+
+    //Handle GET request : search/query parameter
+    if (method === 'GET' && url.includes('/search')) {
+
+        // Ekstrak query parameter dari URL
+        const queryString = url.split('?');
+        const searchParameter = new URLSearchParams(queryString);
+
+        const keyword = searchParameter.get('q') || 'tidak ada keyword';
+        const category = searchParameter.get('category') || 'all';
+
+        response.writeHead(200, { "content-type" : "text/html" });
+        response.end(
+            `
+            <h1>Hasil Pencarian</h1>
+            <p>Keyword: <strong>${keyword}</strong></p>
+            <p>Category: <strong>${category}</strong></p>
+            <br>
+            <a href="/">Kembali ke menu utama</a>
+            `
+        );
+    };
+
+    
 })
