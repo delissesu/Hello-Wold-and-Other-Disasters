db5776688d76cb2b6b0c8cf069d537163f82f555
Delion <21adtydwf@gmail.com>
2025-11-15 17:20:24 +0700

    add: file baru simple crud

---
+++ JavaScript/Project/crud-api.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const { URL } = require('url');
+
+let products = [
+    { id: 1, nama: 'Laptop', price: 5000 },
+    { id: 2, nama: 'Televisi', price: 500 },
+    { id: 3, nama: 'Kulkas', price: 600 },
+    { id: 4, nama: 'Handphone', price: 2000 },
+]
+
