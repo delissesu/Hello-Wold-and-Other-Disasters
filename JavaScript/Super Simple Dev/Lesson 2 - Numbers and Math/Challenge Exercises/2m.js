5dfb79cdef2669b8b3e4411ee3bce983eeca7cd5
Delion <21adtydwf@gmail.com>
2025-12-22 07:41:42 +0700

    convert f to c

---
+++ JavaScript/Super Simple Dev/Lesson 2 - Numbers and Math/Challenge Exercises/2m.js
@@ -0,0 +1,8 @@
+// Given temperature is 86F
+// Convert to C
+
+let temperature = 86
+
+let convert_to_celsius =  (temperature - 32) * 5/9
+
+console.log("The temperature in Celsius is:", convert_to_celsius)
