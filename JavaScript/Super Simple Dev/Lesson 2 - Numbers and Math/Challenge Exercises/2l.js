b197cc311f702b57ce4f2809bf029d8678976460
Delion <21adtydwf@gmail.com>
2025-12-22 07:43:41 +0700

    updating log output in 2l and try to calculate negative temperature and convert to fahrenheit

--- JavaScript/Super Simple Dev/Lesson 2 - Numbers and Math/Challenge Exercises/2l.js
+++ JavaScript/Super Simple Dev/Lesson 2 - Numbers and Math/Challenge Exercises/2l.js
@@ -4,4 +4,4 @@ let temperature = 25
 // Calculate to Fahrenheit
 let convert_to_fahrenheit = (temperature * 9/5) + 32
 
-console.log("The temperate in F is:", convert_to_fahrenheit)
+console.log("The temperate in Fahrenheit is:", convert_to_fahrenheit)
---
+++ JavaScript/Super Simple Dev/Lesson 2 - Numbers and Math/Challenge Exercises/2n.js
@@ -0,0 +1,7 @@
+// Given temperature is -5C
+// Convert to F
+
+let temperature = -5
+let convert_to_fahrenheit = (temperature * 9/5) + 32
+
+console.log("The temperature in Fahrenheit is:", convert_to_fahrenheit)
