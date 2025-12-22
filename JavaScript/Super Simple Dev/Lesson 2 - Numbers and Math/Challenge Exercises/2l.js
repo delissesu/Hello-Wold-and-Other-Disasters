38cb8592ad17894ded45b4f868ec713545f45f19
Delion <21adtydwf@gmail.com>
2025-12-22 07:39:23 +0700

    learning from challenge

---
+++ JavaScript/Super Simple Dev/Lesson 2 - Numbers and Math/Challenge Exercises/2l.js
@@ -0,0 +1,7 @@
+// Given temperature in challenge is 25C
+let temperature = 25
+
+// Calculate to Fahrenheit
+let convert_to_fahrenheit = (temperature * 9/5) + 32
+
+console.log("The temperate in F is:", convert_to_fahrenheit)
