a6c54c145b12c379edc634337660bd3db23c4f79
Delion <21adtydwf@gmail.com>
2025-12-23 10:54:34 +0700

    task 3d and 3f

--- JavaScript/Super Simple Dev/Lesson 3 - String/Exercise/3d.js
+++ JavaScript/Super Simple Dev/Lesson 3 - String/Exercise/3d.js
@@ -4,3 +4,6 @@ let coffe_price = 5
 let bagel_price = 3
 
 console.log(`Total cost: $${coffe_price + bagel_price}`)
+
+// Without variable
+console.log(`Total cost: $${5 + 3}`)
---
+++ JavaScript/Super Simple Dev/Lesson 3 - String/Exercise/3f.js
@@ -0,0 +1,6 @@
+// I must display the result from 3d but using alert in console
+
+alert('Total cost:$'+(5+3)+'')
+
+// With interpolation
+alert(`Total cost: $${5+3}`)
