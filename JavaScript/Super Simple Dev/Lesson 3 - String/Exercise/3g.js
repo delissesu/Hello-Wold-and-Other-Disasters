2634908d667723251f225751c158b545ece3d524
Delion <21adtydwf@gmail.com>
2025-12-23 10:57:49 +0700

    task 3g

---
+++ JavaScript/Super Simple Dev/Lesson 3 - String/Exercise/3g.js
@@ -0,0 +1,8 @@
+// I order a coffe for 5.99 USD and 1 Bagel for 2.95 USD. Use math to calculate and concat
+
+let coffe_price = 599
+let bagel_price = 295
+
+let total_prices = (coffe_price+ bagel_price) / 100
+
+console.log(`Total cost:$${total_prices}`)
