308f65b1535128304ae2374ecc4a1cdb53b44d3b
Delion <21adtydwf@gmail.com>
2025-12-23 17:39:15 +0700

    challenge exc 3l

---
+++ JavaScript/Super Simple Dev/Lesson 3 - String/Challenge Exercises/3l.js
@@ -0,0 +1,18 @@
+// For this task, same like 3k, but different information and math calculation
+// I know the shipping for shirt and basketball is 4.99
+
+// Basketball price and how many ball
+// let basketball_price = 2095
+// let basketball = 2
+// const basketball_final_price = basketball * basketball_price
+
+// Shirt price and shirt piece
+// let shirt_price = 799
+// let shirt = 2
+// const shirt_final_price = shirt * shirt_price
+
+// Shipping order
+let shipping = 499
+let shipping_for_final_order = shipping * 2
+
+console.log(`Shipping & handling: $${shipping_for_final_order / 100}`)
