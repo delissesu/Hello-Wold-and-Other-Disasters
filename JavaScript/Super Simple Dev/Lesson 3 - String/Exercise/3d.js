f7769b0a6cf286ce93043683381b284c5ce904f2
Delion <21adtydwf@gmail.com>
2025-12-23 10:31:19 +0700

    exercise 3d

---
+++ JavaScript/Super Simple Dev/Lesson 3 - String/Exercise/3d.js
@@ -0,0 +1,6 @@
+// I order 1 coffe for 5 USD and 1 bagel for 3 US. Use math to calculate the total cost using concat
+
+let coffe_price = 5
+let bagel_price = 3
+
+console.log(`Total cost: $${coffe_price + bagel_price}`)
