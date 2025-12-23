c6c00cadf14b1ef947c4fe1b8282b12569082d8f
Delion <21adtydwf@gmail.com>
2025-12-23 10:14:47 +0700

    added code i write in dev mode and practice string

---
+++ JavaScript/Super Simple Dev/Lesson 3 - String/code-in-console.js
@@ -0,0 +1,8 @@
+// In here, i learn to how to write code in a string and how to operate with numbers
+alert("Items (" + (1+1) + "): $" + ((2095 + 799) / 100))
+
+// Use double quote to escape single quote
+console.log("I'm Learning Javascript")
+
+// I can use backtick for more clean to insert a value in a string
+conso1e.log(`Items (${1+1}): $${((2095 + 799) / 100)}`)
---
+++ JavaScript/Super Simple Dev/Lesson 3 - String/string.js
@@ -0,0 +1,13 @@
+let text_one = "Delion"
+let text_two = " And Naveria"
+
+// Concat or join the string
+const concat_text = text_one + text_two
+
+console.log(concat_text)
+
+// To check data type, use typeof
+console.log(typeof(concat_text))
+
+// I can use backtick for more clean to insert a value in a string
+console.log(`I use backtick in here: ${text_one + text_two}`)
